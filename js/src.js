05f8ef90b05a4a59aefeaf38b9b9eea0944b1a30
Cassidy <koliadjun@gmail.com>
2019-01-23 20:17:58 +0200

    23012019_1

--- js/src.js
+++ js/src.js
@@ -1,10 +1,14 @@
 
 var page = document.querySelector('.page');
 var pageWrapper = page.querySelector('.wrapper');
-var h1 = page.offsetHeight;
-var h2 = pageWrapper.offsetHeight;
 
+var menu = document.querySelector('.main-nav');
+var menuOpener = document.querySelector('.main-nav__opener');
+
+console.log(menuOpener);
 // console.log( h1, h2)
-if(h1 < h2){
-  page.classList.add('test')
-}
+
+
+menuOpener.addEventListener('click',function(){
+  menu.classList.toggle('is-active');
+})
